28032d09f28f814eeb62a9d86e647b7d6d960d42
Mohamad Alkaadi <mohamadalkaadi@gmail.com>
2023-10-25 04:02:06 +0400

    before frontend

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -27,6 +27,10 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+CORS_ALLOWED_ORIGINS = [
+    "http://127.0.0.1:5173",
+    "http://localhost:5173",
+]
 
 # Application definition
 
@@ -37,17 +41,23 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-]
+    # External Apps
+    'rest_framework',
+    'corsheaders',
+    # Internal Apps
+    'todo',
+  ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    "corsheaders.middleware.CorsMiddleware",
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-]
+  ]
 
 ROOT_URLCONF = 'backend.urls'
 
