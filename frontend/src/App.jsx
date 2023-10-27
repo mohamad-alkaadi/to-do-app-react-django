f0ce78b97a58a0b565050e26129ebc0a31163ba1
Mohamad Alkaadi <mohamadalkaadi@gmail.com>
2023-10-26 22:53:16 -1200

    table component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,17 @@
 import { useState } from 'react'
 import './App.css'
-
+import Table from './components/Table'
+import TodoForm from './components/TodoForm'
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div className='bg-red-500 grid justify-center'>
-      <h1 className=''>Hello world</h1>
+    <div className='bg-indigo-100 px-8 min-h-screen'>
+      <nav className='pt-8'>
+        <h1 className='text-center text-5xl'>To-Do List</h1>
+      </nav>
+      {/* <TodoForm/> */}
+      <Table/>
     </div>
   )
 }
