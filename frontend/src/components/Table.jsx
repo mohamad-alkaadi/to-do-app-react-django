32464777d42f3d574220b14e96f213aca71f70a8
Mohamad Alkaadi <mohamadalkaadi@gmail.com>
2023-10-29 16:44:18 -1200

    dynamic styling

--- frontend/src/components/Table.jsx
+++ frontend/src/components/Table.jsx
@@ -14,21 +14,22 @@ const Table = ({todos, setTodos, isLoading}) => {
                     <th className='p-3 text-sm font-semibold tracking-wide text-left'>Actions</th>
                 </tr>
             </thead>
-            
             <tbody>
             {isLoading? <div>loading</div>:
                 <>
                 {todos.map( (todoItem, index)=> {
                     return (
                         <tr key={todoItem.id} className='border-b border-black'>
-                            <td className='p-3'>
-                                <span className='inline-block cursor-pointer'> <MdOutlineCheckBox/></span>
+                            <td className='p-3' title={todoItem.id}>
+                                <span className='inline-block cursor-pointer'>{todoItem.completed? <MdOutlineCheckBox/>: <MdOutlineCheckBoxOutlineBlank/>} </span>
                             </td>
                             <td className='p-3 text-sm'>{todoItem.body}</td>
                             <td className='p-3 text-sm'>
-                                <span className='p-1.5 text-xs font-medium tracking-wider rounded-md bg-green-300'>Done</span>
+                                <span className={`p-1.5 text-xs font-medium tracking-wider rounded-md ${todoItem.completed? 'bg-green-300': 'bg-red-300'}`}>
+                                    {todoItem.completed?'Done':'Incomplete'}
+                                </span>
                             </td>
-                            <td className='p-3 text-sm'>22-04-23</td>
+                            <td className='p-3 text-sm'>{new Date(todoItem.created).toLocaleString()}</td>
                             <td className='p-3 text-sm font-medium grid grid-flow-col items-center mt-5'>
                                 <span className='text-xl cursor-pointer'><MdEditNote/></span>
                                 <span className='text-xl cursor-pointer'><MdOutlineDeleteOutline/></span>
@@ -36,7 +37,6 @@ const Table = ({todos, setTodos, isLoading}) => {
                         </tr>
                     )
                 })
-                
                 }
                 </>}
             </tbody>
